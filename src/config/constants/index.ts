cb342ef9b9036fd426eabcf2745d056214287333
versedpro <ultraj0330@gmail.com>
2021-09-20 12:05:58 +0300

    feat: Router address

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -1,7 +1,7 @@
 import { ChainId, JSBI, Percent, Token, WETH } from '@pancakeswap/sdk'
 import { BUSD, DAI, USDT, BTCB, CAKE, WBNB, UST, ETH, USDC } from './tokens'
 
-export const ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+export const ROUTER_ADDRESS = '0x2efBEa23f0CD3f80F7802cd3d56E4b53985Fd634'
 
 // a list of tokens by chain
 type ChainTokenList = {
