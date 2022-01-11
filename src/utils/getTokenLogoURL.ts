1e2a1ae42827c8ad086d23f8b19f8cfd7587d66b
versedpro <ultraj0330@gmail.com>
2022-01-11 09:51:42 +0800

    feat(pool): Update logo

--- src/utils/getTokenLogoURL.ts
+++ src/utils/getTokenLogoURL.ts
@@ -1,5 +1,4 @@
-const getTokenLogoURL = (address: string) =>
-  `images/tokens/${address}.png`
-  // `https://assets.trustwalletapp.com/blockchains/smartchain/assets/${address}/logo.png`
+const getTokenLogoURL = (address: string) => `images/tokens/${address}.png`
+// `https://assets.trustwalletapp.com/blockchains/smartchain/assets/${address}/logo.png`
 
 export default getTokenLogoURL
