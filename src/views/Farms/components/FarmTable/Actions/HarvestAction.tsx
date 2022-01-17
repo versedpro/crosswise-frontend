449b3c23bda5bf4138f7d50ff13a77616be34a38
devstar20 <ninjadev2020@gmail.com>
2022-01-17 19:57:18 +0200

    Fix issue of the Crss, XCrss amount

--- src/views/Farms/components/FarmTable/Actions/HarvestAction.tsx
+++ src/views/Farms/components/FarmTable/Actions/HarvestAction.tsx
@@ -18,21 +18,36 @@ import { ActionContainer, ActionTitles, ActionContent, ActionTitlesContainer, Ac
 interface HarvestActionProps extends FarmWithStakedValue {
   userDataReady: boolean
   isAuto?: boolean
+  isVest?: boolean
 }
 
-const HarvestAction: React.FunctionComponent<HarvestActionProps> = ({ pid, userData, userDataReady, isAuto }) => {
+const HarvestAction: React.FunctionComponent<HarvestActionProps> = ({ pid, userData, userDataReady, isVest, isAuto }) => {
   const { toastSuccess, toastError } = useToast()
-  const earningsBigNumber = new BigNumber(userData.earnings)
+  const temp = new BigNumber(userData.earnings)
+  const earningsBigNumber = temp.dividedBy(2);
   const crssPrice = usePriceCrssBusd()
   let earnings = BIG_ZERO
   let earningsBusd = 0
+  let xCrssEarnings = BIG_ZERO
+  let xCrssBusd = 0
   let displayBalance = userDataReady ? earnings.toLocaleString() : <Skeleton width={60} />
+  let displayXcrss = userDataReady ? earnings.toLocaleString() : <Skeleton width={60} />
 
   // If user didn't connect wallet default balance will be 0
   if (!earningsBigNumber.isZero()) {
     earnings = getBalanceAmount(earningsBigNumber)
     earningsBusd = earnings.multipliedBy(crssPrice).toNumber()
     displayBalance = earnings.toFixed(3, BigNumber.ROUND_DOWN)
+    
+    if(isVest){
+      xCrssEarnings = earnings.dividedBy(2)
+      xCrssBusd = xCrssEarnings.multipliedBy(crssPrice).toNumber()
+      displayXcrss = xCrssEarnings.toFixed(3, BigNumber.ROUND_DOWN)
+    }else{
+      xCrssEarnings = earnings
+      xCrssBusd = earningsBusd
+      displayXcrss = xCrssEarnings.toFixed(3, BigNumber.ROUND_DOWN)
+    }
   }
 
   const [pendingTx, setPendingTx] = useState(false)
@@ -80,13 +95,13 @@ const HarvestAction: React.FunctionComponent<HarvestActionProps> = ({ pid, userD
           </ActionTitles>
           <ActionContent>
             <div>
-              <Text fontSize="14px">{displayBalance}</Text>
-              {earningsBusd > 0 && (
+              <Text fontSize="14px">{displayXcrss}</Text>
+              {xCrssBusd > 0 && (
                 <Balance
                   fontSize="12px"
                   color="textSecondary"
                   decimals={2}
-                  value={earningsBusd}
+                  value={xCrssBusd}
                   unit=" USD"
                   prefix="~"
                 />
